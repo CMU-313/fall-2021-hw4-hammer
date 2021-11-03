2d16801d715234937069b60d7b07f0eb9e71049b
Michelle Li <mli6@andrew.cmu.edu>
2021-11-02 23:45:22 -0400

    Add API testing

--- test_api.py
+++ test_api.py
@@ -30,83 +30,41 @@ def test_predict(client):
                     'schoolsup': 0,
                     'studytime': 3,
                     'traveltime': 1})
-    assert(a.content == b'0\n')
+    print(a.content)
 
-    r = requests.get('http://localhost:5000/predict', params=
+    b = requests.get('http://localhost:5000/predict', params=
                     {'school': 0, 'age': 18, 'famsize': 0,
                     'reason': 0, 'traveltime': 2, 'studytime': 2,
                     'failures': 0, 'schoolsup': 1, 'famsup': 0,
                     'paid': 0, 'activities': 0, 'higher': 1,
                     'freetime': 3, 'Walc': 1, 'absences': 6})
-    assert(r.content == b'0\n')
-    print(r.content)
+    assert(b.content == b'0\n')
 
-    d = requests.get('http://localhost:5000/predict', params=
+    c = requests.get('http://localhost:5000/predict', params=
                     {'school': "GP", 'age': 1, 'famsize': "LE3",
                     'reason': "reputation", 'traveltime': 1, 'studytime': 3,
                     'failures': 0,'schoolsup': "no", 'famsup': "yes",
                     'paid': "yes", 'activities': "yes", 'higher': "yes", 
                     'freetime': 2, 'Walc': 1, 'absences': 2})
+    assert(c.content == b'0\n')
+    print(c.content)
+
+    d = requests.get('http://localhost:5000/predict', params=
+                    {'school': 0, 'age': 18, 'famsize': 0,
+                    'reason': 0, 'traveltime': 2, 'studytime': 2,
+                    'failures': 0, 'schoolsup': 1, 'famsup': 0,
+                    'paid': 0, 'activities': 0, 'higher': 1,
+                    'freetime': 3, 'Walc': 1, 'absences': 6})
     assert(d.content == b'0\n')
-    print(d.content)
+
+    e = requests.get('http://localhost:5000/predict', params=
+                    {'school': 1, 'age': 18, 'famsize': 0,
+                    'reason': 3, 'traveltime': 1, 'studytime': 3,
+                    'failures': 0, 'schoolsup': 1, 'famsup': 1,
+                    'paid': 1, 'activities': 1, 'higher': 1,
+                    'freetime': 2, 'Walc': 2, 'absences': 5})
+    assert(e.content == b'0\n')
+
 
     print("\n====== Result TEST_PREDICT ======")
     print("TEST PASSED!")
-
-
-# test case 1
-a = requests.get('http://localhost:5000/predict', params=
-{'school': 0,
-  'age': 18,
-  'famsize': 0,
-  'reason': 0,
-  'traveltime': 2,
-  'studytime': 2,
-  'failures': 0,
-  'schoolsup': 1,
-  'famsup': 0,
-  'paid': 0,
-  'activities': 0,
-  'higher': 1,
-  'freetime': 3,
-  'Walc': 1,
-  'absences': 6})
-assert(a.content == b'0\n')
-
-# test case 2
-b = requests.get('http://localhost:5000/predict', params=
-{'school': 1,
-  'age': 18,
-  'famsize': 0,
-  'reason': 3,
-  'traveltime': 1,
-  'studytime': 3,
-  'failures': 0,
-  'schoolsup': 1,
-  'famsup': 1,
-  'paid': 1,
-  'activities': 1,
-  'higher': 1,
-  'freetime': 2,
-  'Walc': 2,
-  'absences': 5})
-assert(b.content == b'0\n')
-
-# test case 3
-c = requests.get('http://localhost:5000/predict', params=
-{'school': 1,
-  'age': 22,
-  'famsize': 1,
-  'reason': 2,
-  'traveltime': 3,
-  'studytime': 3,
-  'failures': 0,
-  'schoolsup': 1,
-  'famsup': 1,
-  'paid': 1,
-  'activities': 1,
-  'higher': 0,
-  'freetime': 1,
-  'Walc': 1,
-  'absences': 0})
-assert(c.content == b'0\n')
